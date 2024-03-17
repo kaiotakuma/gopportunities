bf863e5a3e670cce681a91ceff698003977cda8d
kaiotakuma <kaiotakuma@gmail.com>
2024-03-17 17:10:20 -0300

    Add routes for opening endpoints

--- router/router.go
+++ router/router.go
@@ -3,12 +3,13 @@ package router
 import "github.com/gin-gonic/gin"
 
 func Initialize() {
+	// Initialize Router
 	router := gin.Default()
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pini",
-		})
-	})
-	// router.Run() // listen and serve on 0.0.0.0:8080
+
+	// Initializes Routes
+
+	InitializeRouter(router)
+	
+	//Run the server
 	router.Run(":8080") // listen and serve on
 }
