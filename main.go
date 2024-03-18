98d81b6d38883d31bc73ef6fc7546e5929daaedf
kaiotakuma <kaiotakuma@gmail.com>
2024-03-18 08:48:42 -0300

    feat: add custom logger

--- main.go
+++ main.go
@@ -1,9 +1,27 @@
 package main
 
 import (
+	"github.com/kaiotakuma/gopportunities/config"
 	"github.com/kaiotakuma/gopportunities/router"
 )
+
+var (
+	logger *config.Logger
+)
+
 func main() {
+
+	logger = config.GetLogger("main")
+
+	// Initialize Configs
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("config initialization error: %v", err)
+		return
+
+	}
+
+	// Initialize Router
 	router.Initialize()
 
 }
